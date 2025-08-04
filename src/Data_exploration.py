8aeadb3cfa25d0450e90c223067e6c823bb4b180
YeruhamMendelson <y0583207549@gmail.com>
2025-08-04 09:57:59 +0300

    class of Data_exploration with names of func

---
+++ src/Data_exploration.py
@@ -0,0 +1,27 @@
+class DataExploration:
+
+    # Basic exploration of the data
+
+    def __init__(self, df, analysis_column,  category):
+        # Gets dataFrame and two columns out of it -
+        # one for investigation, second as a category
+        self.df = df
+        self.analysis_column = analysis_column
+        self.category = category
+
+
+    def num_by_category(self):
+
+        pass
+
+    def len_values(self):
+        pass
+
+    def longest_values(self, num_values):
+        pass
+
+    def common_values(self, num_values):
+        pass
+
+    def upper_case_values(self):
+        pass
